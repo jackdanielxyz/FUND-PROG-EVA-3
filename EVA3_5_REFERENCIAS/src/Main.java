8064ba60df2b3bffcad35cb9a6e668b6982e795a
Dany Perez (Ado's Lover) <143568493+jackdanielxyz@users.noreply.github.com>
2023-11-25 22:59:47 -0600

    Add files via upload

--- EVA3_5_REFERENCIAS/src/Main.java
+++ EVA3_5_REFERENCIAS/src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arreglo = new int [10];
+        System.out.println(arreglo);
+        for (int i : arreglo) {
+            System.out.println(i);
+        }
+        String[][] arreCade = new String[10][10];
+        System.out.println(arreCade);
+        Scanner input = new Scanner(System.in);
+        System.out.println(input);
     }
 }
